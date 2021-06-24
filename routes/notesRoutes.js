e64cc7f17606af4c1983aba7a6f5ae9fc4f0976e
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-06-24 11:28:16 -0400

    add get and post routes

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -1,16 +1,18 @@
 const router = require('express').Router();
-const {notes} = require('../db/db.json');
+const { notes } = require('../db/db');
 const {createNewNote, validateNote} = require('../lib/notes');
 
 router.get('/notes', (req, res) => {
     let results = notes;
+    console.log(results);
     res.json(results);
 })
 
-router.post('./notes', (req, res) => {
+router.post('/notes', (req, res) => {
     if(!validateNote(req.body)) {
         res.status(400).send('The note is not properly formatted');
     } else {
+        
         const note = createNewNote(req.body, notes);
         res.json(note);
     }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const PORT = process.env.port || 3001;
 const app = express();
 const noteRoute = require('./routes/notesRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(express.static('public'));
 
 //use api route
 app.use('/api', noteRoute);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`You are now on port ${PORT}!`)
